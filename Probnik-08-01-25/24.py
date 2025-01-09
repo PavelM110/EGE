5b888c56437d9d6e5b8b12459082b949fcb165a6
PMolchanov <pavel1410071@gmail.com>
2025-01-09 16:02:11 +0200

    Пробник 08.01.25
    Работа над ошибками - 24-решение в разы быстрее, т.к. мы не заставляем аботать цикл кучу раз, а сразу считаем то, что нам надо

--- Probnik-08-01-25/24.py
+++ Probnik-08-01-25/24.py
@@ -4,12 +4,14 @@ with open('24_11636.txt') as file:
 indexes = [i for i in range(len(data)) if data[i] == 'A']
 
 cnt = 0
+l = len(indexes) - 1
 
 for i in range(len(indexes) - 1):
     cnt_in = 0
-    cnt_in += len(indexes[1+i:])
+    cnt_in += l
     if indexes[i+1] - indexes[i] == 1:
         cnt_in -= 1
     cnt += cnt_in
+    l -= 1
 
 print(cnt)
