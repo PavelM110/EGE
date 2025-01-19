83165aaac8a9c0e9dfac05f558e75e7b32048524
PavelM110 <pavel1410071@gmail.com>
2025-01-19 17:12:12 +0300

    https://kompege.ru/variant?kim=25076679
    Task-26-3
    
    Исправлено

--- 18-01-25/Task-26-17565.py
+++ 18-01-25/Task-26-17565.py
@@ -4,19 +4,36 @@ with open('26_17565.txt') as file:
 
 data_sum = [[i[0], sum(i[1:-1]), i[-1]] for i in data]
 
-data_sum = sorted(data_sum, key=lambda x: (-x[1]))
-
+data_sum = sorted(data_sum, key=lambda x: (-x[1], -x[2], x[0]))
 
 prohod = data_sum[:s]
 
-if prohod[-1][1] == data_sum[s][1]:
-    while prohod[-1][1] == data_sum[s][1]:
-        prohod = prohod[:-1]
+cnt = 0
 
-half_prohod = [i for i in data_sum if i[1] == data_sum[s][1]]
+half_prohod = prohod[-1][1]
 
-prohod = sorted(prohod, key=lambda x: (-x[1], -x[-1], -x[0]))
+for i in range(s)[::-1]:
+    if prohod[i][1] != half_prohod:
+        print(prohod[i])
+        break
 
-print(len(half_prohod))
+for i in data_sum:
+    if i[1] == half_prohod:
+        cnt += 1
 
-print(prohod[-1][0])
+print(cnt)
+
+
+# prohod = data_sum[:s]
+#
+# if prohod[-1][1] == data_sum[s][1]:
+#     while prohod[-1][1] == data_sum[s][1]:
+#         prohod = prohod[:-1]
+#
+# half_prohod = [i for i in data_sum if i[1] == data_sum[s][1]]
+#
+# prohod = sorted(prohod, key=lambda x: (-x[1], -x[-1], -x[0]))
+#
+# print(len(half_prohod))
+#
+# print(prohod[-1][0])
