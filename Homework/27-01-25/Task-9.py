244d93492d9cd0aad0ecf8ef4b6dedfe2fae2f76
PavelM110 <pavel1410071@gmail.com>
2025-01-25 18:12:32 +0300

    Task-9

---
+++ Homework/27-01-25/Task-9.py
@@ -0,0 +1,16 @@
+with open('9_11946.txt') as file:
+    data = [list(map(int, i.split())) for i in file if i]
+
+def f1(arr):
+    count = [arr.count(i) for i in arr]
+    return count.count(3) == 3 and count.count(1) == 4
+
+def f2(arr):
+    return arr == sorted(arr)
+
+cnt = 0
+
+for i in data:
+    if f1(i) and f2(i): cnt += 1
+
+print(cnt)
