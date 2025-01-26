575ac824c779b722a1f51edad4421c0d7d36dfea
PavelM110 <pavel1410071@gmail.com>
2025-01-26 13:55:27 +0300

    Task-15
    ИСПРАВЛЕНО

--- Homework/27-01-25/Task-15.py
+++ Homework/27-01-25/Task-15.py
@@ -1,15 +1,14 @@
-def s(r, b, c):
-    return (r * b) > c
+def s(a, b, c):
+    return a*b > c
 
-def f(t):
+def f(a):
     for x in range(1, 1000):
         for y in range(1, 1000):
-            u = (not s(x, y, t + 13)) <= (s(28, y, 520) or s(x, 25, 800))
-            if not u:
-                return False
+            u =  (not s(x, y, a+13)) <= (s(28, y, 520) or (s(x, 25, 800)))
+            if not u: return False
     return True
 
-for a in range(1, 100000)[::-1]:
+for a in range(-100, 100)[::-1]:
     if f(a):
         print(a)
         break
