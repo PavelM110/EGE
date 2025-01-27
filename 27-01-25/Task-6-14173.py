eb5b66956898db5112f5d2392d9c5cadc5c84618
PMolchanov <pavel1410071@gmail.com>
2025-01-27 16:39:41 +0200

    https://kompege.ru/variant?kim=25078582
    Task-6

---
+++ 27-01-25/Task-6-14173.py
@@ -0,0 +1,34 @@
+from turtle import *
+
+tracer(0)
+lt(90)
+up()
+m = 10
+
+for i in range(2):
+    down()
+    for j in range(2):
+        fd(8 * m)
+        rt(90)
+        fd(8 * m)
+        rt(90)
+    up()
+    fd(6 * m)
+    rt(90)
+    fd(6 * m)
+    lt(90)
+rt(180)
+fd(4 * m)
+down()
+for i in range(4):
+    fd(8 * m)
+    rt(270)
+
+up()
+
+for x in range(-10, 30):
+    for y in range(-10, 30):
+        goto(x * m, y * m)
+        dot(4, 'white')
+
+done()
