cd1b67d68f5bff824d35b8fa5f3002806fd7ebce
PMolchanov <pavel1410071@gmail.com>
2025-02-06 17:06:46 +0200

    https://kompege.ru/variant?kim=25080927
    Task-15-1

--- 06-02-25/Task-15-748.py
+++ 06-02-25/Task-15-748.py
@@ -1,2 +1,16 @@
 from itertools import combinations
 
+def f(x):
+    p = 44 <= x <= 49
+    q = 28 <= x <= 53
+    a = a1 <= x <= a2
+    u = (a <= p) or q
+    return u
+
+ans = []
+line = [0, 44, 49, 28, 53, 60]
+
+for a1, a2 in combinations(sorted(line), 2):
+    if all([f(x) for x in range(0, 60)]): ans.append(a2 - a1)
+
+print(max(ans))
