6ab2004629c77dcb62cb867052624850daa2b0d9
PMolchanov <pavel1410071@gmail.com>
2025-02-06 16:38:10 +0200

    https://kompege.ru/variant?kim=25080912
    Task-15-1

--- 06-02-25/Task-15-11665.py
+++ 06-02-25/Task-15-11665.py
@@ -2,11 +2,11 @@ def m(m, n): return m % n
 
 def f(a):
     for x in range(1, 1000):
-        u = (a+x > 700-a) or (m(a, 100) + m(100, x) > 50)
+        u = (a+x > 700-a) and (m(a, 100) + m(100, x) > 50)
         if not u: return False
     return True
 
-for a in range(1, 100):
+for a in range(1, 500):
     if f(a):
         print(a)
         break
