6e54df9973c5005416917727c5dbe4cb862dee8d
PMolchanov <pavel1410071@gmail.com>
2025-02-06 16:20:19 +0200

    https://kompege.ru/variant?kim=25080912
    Task-1

---
+++ 06-02-25/Task-1-17664.py
@@ -0,0 +1,10 @@
+from itertools import permutations
+
+matrix = '248 157 456 136 23 34 28 17'.split()
+graph = 'fc ch hb be ea af cg gd dh ab'.split()
+
+print(*range(1, 9))
+
+for p in permutations('abcdefgh'):
+    if all(str(p.index(x) + 1) in matrix[p.index(y)] for x, y in graph):
+        print(*p)
