b43b37ba4fd1792caac4d1416d0d43e5c12d2d3b
PMolchanov <pavel1410071@gmail.com>
2025-02-10 17:53:40 +0200

    https://kompege.ru/variant?kim=25081650
    Task-17-9

---
+++ Lessons/Year-25/February/10-02-25/Task-17-17680.py
@@ -0,0 +1,14 @@
+with open('17_17680.txt') as file:
+    data = [int(i) for i in file if i]
+
+min_41 = min(i for i in data if i > 0 and i % 41 == 0)
+
+ans = []
+
+for i in range(len(data) - 1):
+    a, b = data[i:i+2]
+    u1 = len((a, b)) == len(set((a, b)))
+    u2 = abs(a - b) % min_41 == 0
+    if u1 and u2: ans.append(a+b)
+
+print(len(ans), max(ans))
