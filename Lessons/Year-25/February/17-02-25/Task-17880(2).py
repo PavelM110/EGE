1f212c07f16f3d12bf3213a7f7a185dc5b5d285d
PMolchanov <pavel1410071@gmail.com>
2025-02-17 17:46:26 +0200

    https://kompege.ru/variant?kim=25083166
    Task-25-3

--- Lessons/Year-25/February/17-02-25/Task-17880(2).py
+++ Lessons/Year-25/February/17-02-25/Task-17880(2).py
@@ -2,4 +2,4 @@ from fnmatch import fnmatch
 
 for i in range(30120145 - 30120145 % 1917, 10**10, 1917):
     if fnmatch(str(i), '3?12?14*5'):
-        print(i, i // 19174)
+        print(i, i // 1917)
