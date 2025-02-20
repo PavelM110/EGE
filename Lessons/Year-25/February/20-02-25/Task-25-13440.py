a485a448d511af8c74bd9ca14ec6c22fb4efed74
PMolchanov <pavel1410071@gmail.com>
2025-02-20 16:43:49 +0200

    https://kompege.ru/variant?kim=25083844
    Task-25-1

--- Lessons/Year-25/February/20-02-25/Task-25-13440.py
+++ Lessons/Year-25/February/20-02-25/Task-25-13440.py
@@ -6,7 +6,7 @@ ans = []
 for v in '0123456789':
     for r in range(5):
         for z in product('0123456789', repeat=r):
-            num = int(f'85{''.join(z)}16{v}4')
+            num = int(f'85{v}16{''.join(z)}4')
             if num % 2658 == 0 and num < 10**9: ans.append((num, num // 2658))
 
 for i in sorted(ans):
@@ -17,7 +17,7 @@ print('')
 ans1 = []
 
 for num in range(851604 - 851604 % 2658, 10**9, 2658):
-    if fnmatch(str(num), '85*16?4'): ans1.append((num, num // 2658))
+    if fnmatch(str(num), '85?16*4'): ans1.append((num, num // 2658))
 
 for i in sorted(ans1):
     print(*i)
