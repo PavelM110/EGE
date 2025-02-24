fbc73c9640eca34586ebffd6bd32eba7dab63805
PMolchanov <pavel1410071@gmail.com>
2025-02-24 17:36:42 +0200

    https://kompege.ru/variant?kim=25084536
    Task-25-3

---
+++ Lessons/Year-25/February/24-02-25/Task-25-17564.py
@@ -0,0 +1,21 @@
+def f(n):
+    divs = []
+    for i in range(2, int(n ** .5) + 1):
+        if n % i == 0:
+            divs.append(i)
+            divs.append(n // i)
+    divs = set(divs)
+    if divs:
+        summ = max(divs) + min(divs)
+        return summ
+    return 0
+
+cnt = 0
+
+for i in range(700_001, 10**16):
+    m = f(i)
+    if m % 10 == 4:
+        print(i, m)
+        cnt += 1
+        if cnt == 5:
+            break
