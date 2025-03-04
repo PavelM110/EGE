abd55fb7f55312935e6d0ff675dbcf156b80555b
PavelM110 <pavel1410071@gmail.com>
2025-03-04 17:29:25 +0300

    https://kompege.ru/variant?kim=25086062
    Task-16-3

---
+++ Homework/March/06-03-25/Task-16-18866.py
@@ -0,0 +1,12 @@
+from functools import lru_cache
+
+@lru_cache(None)
+def f(n):
+    if n < 100: return n ** 2
+    if n % 2 == 0: return f(n - 1) // 2
+    return 2 * f(n - 1)
+
+for i in range(16385):
+    f(i)
+
+print(1000 * f(16384) // f(7777))
