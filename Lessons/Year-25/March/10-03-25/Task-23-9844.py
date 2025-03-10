2564fa1b827f8d50c9f39f669924587556b00c19
PMolchanov <pavel1410071@gmail.com>
2025-03-10 17:55:40 +0200

    https://kompege.ru/variant?kim=25087370
    Task-23-9-extra

--- Lessons/Year-25/March/10-03-25/Task-23-9844.py
+++ Lessons/Year-25/March/10-03-25/Task-23-9844.py
@@ -1,6 +1,17 @@
-def f(s, e):
-    if s == e: return 1
-    if s < e or s == 7: return 0
-    return f(s - 1, e) + f(s - 3, e) + f(s // 2, e)
+last_step = 0
 
-print(f(19, 10) * f(10, 3))
+def f(s, e, c):
+    global last_step
+    if s == e:
+        last_step = c
+        return 1
+    if s < e or s == 7: return 0
+    if c == 1:
+        return f(s - 3, e, 2) + f(s // 2, e, 3)
+    elif c == 2:
+        return f(s - 1, e, 1) + f(s // 2, e, 3)
+    elif c == 3:
+        return f(s - 1, e, 1) + f(s - 3, e, 2)
+    return f(s - 1, e, 1) + f(s - 3, e, 2) + f(s // 2, e, 3)
+
+print(f(19, 10, 0) * f(10, 3, last_step))
