b3307e345518eaed5b80644dda99a12685dc0fbb
PavelM110 <pavel1410071@gmail.com>
2025-03-11 18:02:46 +0300

    https://kompege.ru/variant?kim=25087422
    Task-16-1

---
+++ Homework/March/13-03-25/Task-16-19709.py
@@ -0,0 +1,11 @@
+from functools import lru_cache
+
+@lru_cache(None)
+def f(n):
+    if n == 1: return 1
+    return n ** 3 + f(n - 1)
+
+for i in range(1, 2026):
+    f(i)
+
+print(f(2025) - f(2022))
