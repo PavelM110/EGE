c8fa539d022a2bea13387be89980dea02ce74f2e
PavelM110 <pavel1410071@gmail.com>
2025-03-11 18:06:50 +0300

    https://kompege.ru/variant?kim=25087422
    Task-15-1

---
+++ Homework/March/13-03-25/Task-15-8596.py
@@ -0,0 +1,11 @@
+def f(a):
+    for x in range(1, 1000):
+        for y in range(1, 1000):
+            u = (x >= 11) or (3*x < y) or (x * y < a)
+            if not u: return False
+    return True
+
+for a in range(1, 1000):
+    if f(a):
+        print(a)
+        break
