d4be78f8370b9ef0eec7d78a129417d4171c753e
PMolchanov <pavel1410071@gmail.com>
2025-03-22 19:45:08 +0200

    https://kompege.ru/variant?kim=25088657
    Task-26-2
    
    Rename

--- Lessons/Year-25/March/22-03-25/Task-26-4553.py
+++ Lessons/Year-25/March/22-03-25/Task-26-4553.py
@@ -2,27 +2,27 @@ with open('26_4553.txt') as file:
     n = int(file.readline())
     data = [(int(i.split()[0]), int(i.split()[1]), i.split()[2]) for i in file if i]
 
-m = ['0'*10001 for i in range(10002)]
+matrix = ['0'*10001 for i in range(10002)]
 
 for y, x, z in data:
-    st = m[y]
+    st = matrix[y]
     if z == '+':
         st = st[:x] + '1' + st[x+1:]
     else:
         st = st[:x] + '0' + st[x + 1:]
-    m[y] = st
+    matrix[y] = st
 
-m1 = []
+matrix1 = []
 ans1 = 0
 ans2 = 0
 
-for i in m:
+for i in matrix:
     i = i.replace('0', ' ')
     i = i.split()
-    m1.append(i)
+    matrix1.append(i)
 
-for i in range(len(m1)):
-    n = m1[i]
+for i in range(len(matrix1)):
+    n = matrix1[i]
     if n and ans1 <= len(max(n, key=len)):
         ans1 = len(max(n, key=len))
         ans2 = i
