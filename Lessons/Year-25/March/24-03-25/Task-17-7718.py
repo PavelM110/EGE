1f4a2c0c1ca95aa49d0e1d854444df154142f864
PMolchanov <pavel1410071@gmail.com>
2025-03-24 16:39:40 +0200

    https://kompege.ru/variant?kim=25090443
    Task-17-7718

--- Lessons/Year-25/March/24-03-25/Task-17-7718.py
+++ Lessons/Year-25/March/24-03-25/Task-17-7718.py
@@ -8,7 +8,7 @@ ans = []
 for a, b in combinations(data, 2):
     summ = a + b
     prod = a * b
-    if summ % 18 == 0 or prod % 18 == 0:
+    if (summ % 18 == 0) ^ (prod % 18 == 0):
         ans.append(summ)
 
 print(len(ans), max(ans))
