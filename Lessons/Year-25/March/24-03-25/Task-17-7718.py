979965b4a21766dc428ca917a2b8981ef1f04e12
PMolchanov <pavel1410071@gmail.com>
2025-03-24 16:15:59 +0200

    https://kompege.ru/variant?kim=25090443
    Task-17-7718

---
+++ Lessons/Year-25/March/24-03-25/Task-17-7718.py
@@ -0,0 +1,14 @@
+from itertools import combinations
+
+with open('17 (2)_7718.txt') as file:
+    data = [int(i) for i in file if i]
+
+ans = []
+
+for a, b in combinations(data, 2):
+    summ = a + b
+    prod = a * b
+    if summ % 18 == 0 or prod % 18 == 0:
+        ans.append(summ)
+
+print(len(ans), max(ans))
