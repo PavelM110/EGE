c3479335c2c3344aebc585bedb8820ff339392ee
PMolchanov <pavel1410071@gmail.com>
2025-03-25 12:10:14 +0200

    Probnik https://kompege.ru/variant?kim=25090569
    Task-14

---
+++ Probnik-25-03-25/Task-14.py
@@ -0,0 +1,11 @@
+ans = []
+
+for x in range(1, 2030):
+    num = 2**2025 + 2*2024 - 2**2021 - x
+    cnt = 0
+    while num:
+        if num % 4 == 0: cnt += 1
+        num //= 4
+    ans.append((cnt, x))
+
+print(sorted(ans, key=lambda x: (-x[0], -x[1])))
