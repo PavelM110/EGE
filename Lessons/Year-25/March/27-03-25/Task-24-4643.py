2f6ce5420baeedc635459c1c82dfb08bf621065d
PMolchanov <pavel1410071@gmail.com>
2025-03-27 16:24:05 +0200

    https://kompege.ru/variant?kim=25091042
    Task-24

--- Lessons/Year-25/March/27-03-25/Task-24-4643.py
+++ Lessons/Year-25/March/27-03-25/Task-24-4643.py
@@ -7,6 +7,8 @@ data = data.replace('2', '1').replace('B', 'A')
 
 data = data.replace('11A', '*')
 
+print(len(max([list(g) for k, g in groupby(data) if k == '*'], key=len)))
+
 data = data.replace('1', ' ').replace('A', ' ')
 
 data = data.split()
