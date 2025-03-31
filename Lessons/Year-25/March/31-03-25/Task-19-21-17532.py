bad79c2043e79de6e2e65268569fa0b2b93d06bc
PMolchanov <pavel1410071@gmail.com>
2025-03-31 17:32:51 +0200

    https://kompege.ru/variant?kim=25090444
    Task-19-21-1

---
+++ Lessons/Year-25/March/31-03-25/Task-19-21-17532.py
@@ -0,0 +1,25 @@
+def f(x, y, s):
+    if x + y >= 65: return s % 2 == 0
+    if s == 0: return False
+    h = [
+        f(x + 1, y, s - 1),
+        f(x * 3, y, s - 1),
+        f(x, y + 1, s - 1),
+        f(x, y * 3, s - 1)
+    ]
+    return any(h) if (s - 1) % 2 == 0 else all(h)
+
+def d(x, y, s):
+    if x + y >= 65: return s % 2 == 0
+    if s == 0: return False
+    h = [
+        d(x + 1, y, s - 1),
+        d(x * 3, y, s - 1),
+        d(x, y + 1, s - 1),
+        d(x, y * 3, s - 1)
+    ]
+    return any(h)
+
+print('19)', [s for s in range(1, 59) if d(s, 6, 2)])
+print('20)', [s for s in range(1, 59) if f(s, 6, 3) and not f(s, 6, 1)])
+print('21)', [s for s in range(1, 59) if f(s, 6, 4) and not f(s, 6, 2)])
