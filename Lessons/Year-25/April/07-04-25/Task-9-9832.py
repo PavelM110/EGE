6acba6141ed82c310fbd2eb4fe1198bef10bbf84
PMolchanov <pavel1410071@gmail.com>
2025-04-07 16:27:00 +0200

    https://kompege.ru/variant?kim=25063517
    Task-9-5

---
+++ Lessons/Year-25/April/07-04-25/Task-9-9832.py
@@ -0,0 +1,14 @@
+with open('9_9832.txt') as file:
+    data =  [list(map(int, i.split())) for i in file if i]
+
+def f1(a):
+    cnt = [a.count(i) for i in set(a)]
+    return cnt.count(2) == 2 and cnt.count(1) == 3
+
+def f2(a):
+    return a.count(max(a)) == 1
+
+for i in data:
+    if f1(i) and f2(i):
+        print(sum(i))
+        break
