9ed3489f4b303a7ff5154fcd5bbd62c7cca49084
PavelM110 <pavel1410071@gmail.com>
2025-04-09 19:40:08 +0300

    https://kompege.ru/variant?kim=25093291
    Task-16

---
+++ Homework/April/Variant-7-13-April-25/Task-16-5168.py
@@ -0,0 +1,13 @@
+from functools import lru_cache
+
+@lru_cache(None)
+def f(n):
+    if n >= 10000: return n
+    if n % 2 == 0:
+        return f(n + 1) + n ** 2 - 3 * (n - 1)
+    return f(n + 2) + 5 * n - (n - 1)
+
+for i in range(10010, 89, -1):
+    f(i)
+
+print(f(90) - f(99))
