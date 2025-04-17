2798b4dc76b34377e29afe63dc770d9db5109264
PMolchanov <pavel1410071@gmail.com>
2025-04-17 16:43:17 +0200

    https://kompege.ru/variant?kim=25093689
    Task-13-7

---
+++ Lessons/Year-25/April/17-04-25/Task-13-11791.py
@@ -0,0 +1,10 @@
+from ipaddress import ip_network
+from itertools import product
+
+a_list = [int(''.join(i), 2) for i in product('01', repeat=8) if '01' not in ''.join(i)]
+
+for a in a_list:
+    net = ip_network(f'246.51.128.202/255.255.{a}.0', False)
+    if all(f'{int(i):032b}'[:16].count('0') <= f'{int(i):032b}'[17:].count('0') for i in net):
+            print(a)
+            break
