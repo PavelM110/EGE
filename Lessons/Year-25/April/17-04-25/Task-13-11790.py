d11cac9ac6d5c151ff1838e9a8503b537b452def
PMolchanov <pavel1410071@gmail.com>
2025-04-17 17:04:56 +0200

    https://kompege.ru/variant?kim=25093689
    Mistakes

--- Lessons/Year-25/April/17-04-25/Task-13-11790.py
+++ Lessons/Year-25/April/17-04-25/Task-13-11790.py
@@ -1,8 +1,7 @@
 from ipaddress import ip_network
-from itertools import product
 
 for a in range(16, 25):
     net = ip_network(f'152.65.245.132/{a}', False)
     if all(f'{int(i):032b}'[:16].count('0') >= f'{int(i):032b}'[16:].count('0') for i in net):
-        print(int('1'*(a - 16) + '0'*(8 - (a - 16)), 2))
+        print(net.netmask)
         break
