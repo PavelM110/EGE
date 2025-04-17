69c59b84f619e45108a87647422bcd260fa3a781
PMolchanov <pavel1410071@gmail.com>
2025-04-17 17:47:18 +0200

    https://kompege.ru/variant?kim=25093899
    Task-13-6
    
    Или так

--- Lessons/Year-25/April/17-04-25/Task-13-10782.py
+++ Lessons/Year-25/April/17-04-25/Task-13-10782.py
@@ -1,12 +1,11 @@
 from ipaddress import ip_network, ip_address
 
-for a in range(1, 33)[::-1]:
+for a in range(1, 31)[::-1]:
     ip1 = ip_address('118.187.59.255')
     ip2 = ip_address('118.187.65.115')
     net1 = ip_network(f'118.187.59.255/{a}', False)
     net2 = ip_network(f'118.187.65.115/{a}', False)
     if net1.network_address != net2.network_address:
-        if ip1 != net1.broadcast_address and ip1 != net1.network_address:
-            if ip2 != net2.broadcast_address and ip2 != net2.network_address:
-                print(a)
+        if ip1 in net1.hosts():
+            if ip2 in net2.hosts():
                 break
