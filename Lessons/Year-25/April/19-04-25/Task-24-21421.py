e4196d35b3dde6671a6d0adccaa61efa89b0f8fb
PMolchanov <pavel1410071@gmail.com>
2025-04-19 18:44:22 +0200

    https://kompege.ru/task
    Task-24-21421

--- Lessons/Year-25/April/19-04-25/Task-24-21421.py
+++ Lessons/Year-25/April/19-04-25/Task-24-21421.py
@@ -3,7 +3,7 @@ from re import *
 with open('24_21421.txt') as file:
     data = file.readline()
 
-pat = r'[1-9][0-9AB]*[02468A]'
+pat = r'[1-9AB][0-9AB]*[02468A]'
 
 matches = finditer(pat, data)
 
