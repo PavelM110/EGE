06eb2ad1789a8c32f4309fa44ad868b86630b110
PMolchanov <pavel1410071@gmail.com>
2025-04-19 19:29:06 +0200

    RE

--- Homework/April/Variant-21-04-25/Task-24-19717.py
+++ Homework/April/Variant-21-04-25/Task-24-19717.py
@@ -3,16 +3,23 @@ from re import *
 with open('24.5_19717.txt') as file:
     data = file.readline()
 
-pat = r'(?<=[M/A])([AEGILR]*+M){278}[AEGILR]*+(?=[M/Z])'
+not_M = r'([^M]*)'
+M_278 = r'(?:M[^M]*){278}'
+la_278_M = rf'(?=({M_278}))'
+pat = rf'{not_M}{la_278_M}'
 
-matches = finditer(pat, data)
+matches = findall(pat, data)
 
-ans = 0
+# ans = 0
+#
+# for match in matches:
+#     ans = max(ans, len(match.group()))
+#
+# print(ans)
 
-for match in matches:
-    ans = max(ans, len(match.group()))
-
-print(ans)
+res = max([len(match[0] + match[1])
+           for match in matches])
+print(res)
 
 ans = 0
 
