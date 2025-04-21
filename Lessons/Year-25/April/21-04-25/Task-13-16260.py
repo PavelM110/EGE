dc2b21a71b5048f4435a66786a0aa35a01f8eab2
PMolchanov <pavel1410071@gmail.com>
2025-04-21 17:10:40 +0200

    https://kompege.ru/variant?kim=25094484
    Task-13-8

--- Lessons/Year-25/April/21-04-25/Task-13-16260.py
+++ Lessons/Year-25/April/21-04-25/Task-13-16260.py
@@ -3,10 +3,10 @@ from ipaddress import ip_address, ip_network
 ip1 = ip_address('123.20.103.136')
 ip2 = ip_address('123.20.103.151')
 
-for a in range(33):
+for a in range(31):
     net1 = ip_network(f'{ip1}/{a}', False)
     net2 = ip_network(f'{ip2}/{a}', False)
     if net1.network_address != net2.network_address:
-        if ip1 != net1.broadcast_address and ip1 != net1.network_address and \
-            ip2 != net2.broadcast_address and ip2 != net2.network_address:
+        if ip1 in net1.hosts() and \
+            ip2 in net2.hosts():
             print(net1.netmask)
