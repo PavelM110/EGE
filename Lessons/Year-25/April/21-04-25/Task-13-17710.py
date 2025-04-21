5933d62726d81daf4800e739708c1f9dd6aca273
PMolchanov <pavel1410071@gmail.com>
2025-04-21 16:16:43 +0200

    https://kompege.ru/variant?kim=25094282
    Task-13

---
+++ Lessons/Year-25/April/21-04-25/Task-13-17710.py
@@ -0,0 +1,12 @@
+from ipaddress import ip_network
+
+net = ip_network('214.187.224.0/255.255.224.0', False)
+
+cnt = 0
+
+for i in net:
+    i = f'{int(i):032b}'
+    if i.count('1') % 6 != 0 and i[-4:] != '1000':
+        cnt += 1
+
+print(cnt)
