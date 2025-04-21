8af246093de2baf5f109b4e244c235b50a84b582
PMolchanov <pavel1410071@gmail.com>
2025-04-21 17:45:19 +0200

    https://kompege.ru/variant?kim=25095201
    Task-13-9

---
+++ Lessons/Year-25/April/21-04-25/Task-13-10783.py
@@ -0,0 +1,12 @@
+from ipaddress import ip_network, ip_address
+
+ip1 = ip_address('121.171.5.70')
+ip2 = ip_address('121.171.5.107')
+
+for a in range(31)[::-1]:
+    net1 = ip_network(f'{ip1}/{a}', False)
+    net2 = ip_network(f'{ip2}/{a}', False)
+    if net1.network_address == net2.network_address:
+        if ip1 in net1.hosts() and ip2 in net2.hosts():
+            print(net1.num_addresses)
+            break
