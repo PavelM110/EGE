8a336abd97aec4ce56040d79aeaf7edc3af33df6
PMolchanov <pavel1410071@gmail.com>
2025-04-24 16:39:03 +0200

    https://kompege.ru/variant?kim=25095191
    Task-8-2

--- Lessons/Year-25/April/24-04-25/Task-8-4588.py
+++ Lessons/Year-25/April/24-04-25/Task-8-4588.py
@@ -3,11 +3,10 @@ from itertools import product
 cnt = 0
 
 for i in product('01234567', repeat=5):
-    if i[0] != '0':
-        i = ''.join(i)
-        if i.count('6') == 1:
-            for j in '024': i = i.replace(j, '6')
-            if '66' not in i:
-                cnt += 1
+    i = ''.join(i)
+    if i.count('6') == 1 and i[0] != '0':
+        for j in '137': i = i.replace(j, '5')
+        if '56' not in i and '65' not in i:
+            cnt += 1
 
 print(cnt)
