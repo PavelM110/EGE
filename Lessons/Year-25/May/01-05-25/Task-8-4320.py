741c039092e295d4c488e73737514f07d26bdf56
PMolchanov <pavel1410071@gmail.com>
2025-05-01 16:03:50 +0200

    https://kompege.ru/variant?kim=25097415
    Task-8

---
+++ Lessons/Year-25/May/01-05-25/Task-8-4320.py
@@ -0,0 +1,15 @@
+from itertools import permutations
+
+cnt = 0
+
+for i in permutations('01234567'):
+    if i[0] != '0':
+        i = ''.join(i)
+        st = i
+        for j in '357': st = st.replace(j, '1')
+        for j in '246': st = st.replace(j, '0')
+        if '11' not in st and '00' not in st:
+            if int(i, 8) % 5 == 0:
+                cnt += 1
+
+print(cnt)
