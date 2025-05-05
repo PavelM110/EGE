6d8ca53c1e6f49a550cea815a1707563097395fa
PMolchanov <pavel1410071@gmail.com>
2025-05-05 17:40:38 +0200

    https://kompege.ru/variant?kim=25097992
    Task-27-3

---
+++ Lessons/Year-25/May/05-05-25/Task-27-21932.py
@@ -0,0 +1,32 @@
+from math import dist
+
+with open('27_B_21932.txt') as file:
+    data = [list(map(float, i.split())) for i in file if i]
+
+def centroid(cluster):
+    dists = []
+    for dot in cluster:
+        sum_d = 0
+        for dot2 in cluster:
+            sum_d += dist(dot, dot2)
+        dists.append([sum_d, dot])
+    return min(dists)[1]
+
+eps = 1
+clusters = []
+
+while data:
+    cluster = [data.pop()]
+    for dot in cluster:
+        for dot2 in data.copy():
+            if dist(dot, dot2) < eps:
+                cluster.append(dot2)
+                data.remove(dot2)
+    clusters.append(cluster)
+
+print([len(i) for i in clusters])
+
+center_x = centroid(min(clusters, key=len))[0]
+center_y = centroid(max(clusters, key=len))[1]
+
+print(int(center_x * 10_000), int(center_y * 10_000))
