bfd54c3aca2d8e3fe7151a289e0a95fe53517257
PavelM110 <pavel1410071@gmail.com>
2025-05-11 14:40:26 +0300

    https://kompege.ru/variant?kim=25099163
    Task-19-21

---
+++ Homework/May/Variant-12-05-25/Task-19-21-9072.py
@@ -0,0 +1,17 @@
+def f(x, s):
+    if x >= 100: return s % 2 == 0
+    if s == 0: return False
+    h = [f(x + 7, s - 1),
+         f(x * 2, s - 1)]
+    return any(h) if (s - 1) % 2 == 0 else all(h)
+
+def d(x, s):
+    if x >= 100: return s % 2 == 0
+    if s == 0: return False
+    h = [d(x + 7, s - 1),
+         d(x * 2, s - 1)]
+    return any(h) if (s - 1) % 2 == 0 else any(h)
+
+print('19)', [s for s in range(1, 100) if f(s, 1) and d(s, 2)])
+print('20)', [s for s in range(1, 100) if f(s, 2) and not f(s, 3) and d(s, 3)])
+print('21)', [s for s in range(1, 100) if f(s, 4) and not f(s, 2)])
