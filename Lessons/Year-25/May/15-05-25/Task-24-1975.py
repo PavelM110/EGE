6d9b1569f9770d5b6731c72b94a9adbcc0644891
PMolchanov <pavel1410071@gmail.com>
2025-05-15 17:00:04 +0200

    https://kompege.ru/variant?kim=25099953
    Task-24-1

--- Lessons/Year-25/May/15-05-25/Task-24-1975.py
+++ Lessons/Year-25/May/15-05-25/Task-24-1975.py
@@ -3,14 +3,14 @@ from re import *
 with open('24_1975.txt') as file:
     data = file.readline()
 
-pat = r'[^R]*(R[^R]+)+R?'
+pat = r'[^P]*(P[^P]+)+P?'
 
 
 matches = finditer(pat, data)
 
 print(len(max([i.group() for i in matches], key=len)))
 
-data = data.replace('RR', 'R R')
+data = data.replace('PP', 'P P')
 data = data.split()
 
 print(len(max(data, key=len)))
