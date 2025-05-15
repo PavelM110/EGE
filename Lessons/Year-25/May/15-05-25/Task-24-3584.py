ebc7c7d2beac419c5d09edc04874fb04d46a32a5
PMolchanov <pavel1410071@gmail.com>
2025-05-15 17:25:02 +0200

    https://kompege.ru/variant?kim=25099953
    Task-24-2-4

--- Lessons/Year-25/May/15-05-25/Task-24-3584.py
+++ Lessons/Year-25/May/15-05-25/Task-24-3584.py
@@ -1,5 +1,16 @@
 from re import *
 
-with open('Task-24-3584.py') as file:
+with open('24_3584.txt') as file:
     data = file.readline()
 
+pat = r'((BA)|(DA))+'
+
+matches = finditer(pat, data)
+
+print(len(max([i.group() for i in matches], key=len)) // 2)
+
+data = data.replace('BA', '@').replace('DA', '@')
+for i in 'ABD': data = data.replace(i, ' ')
+data = data.split()
+
+print(len(max(data, key=len)))
