93a8fd477c087adc39e4937b23d356497e555206
PMolchanov <pavel1410071@gmail.com>
2025-05-15 17:20:00 +0200

    https://kompege.ru/variant?kim=25099953
    Task-24-2-4

---
+++ Lessons/Year-25/May/15-05-25/Task-24-1866.py
@@ -0,0 +1,10 @@
+from re import *
+
+with open('24_1866.txt') as file:
+    data = file.readline()
+
+data = data.replace('ad', 'a d').replace('da', 'd a')
+
+data = data.split()
+
+print(len(max(data, key=len)))
---
+++ Lessons/Year-25/May/15-05-25/Task-24-3584.py
@@ -0,0 +1,5 @@
+from re import *
+
+with open('Task-24-3584.py') as file:
+    data = file.readline()
+
