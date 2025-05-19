5798e616b4da5ab298e6b3507609504059695ebe
PMolchanov <pavel1410071@gmail.com>
2025-05-19 16:54:05 +0200

    https://kompege.ru/variant?kim=25099956
    Task-24-10

---
+++ Lessons/Year-25/May/19-05-25/Task-24-9791.py
@@ -0,0 +1,25 @@
+from string import ascii_uppercase
+from re import *
+
+with open('24_9791 (1).txt') as file:
+    data = file.readline()
+
+
+pat = r'[1-9A-F][\dA-F]*'
+
+matches = finditer(pat, data)
+
+print(max([len(i.group()) for i in matches]))
+
+
+for i in ascii_uppercase[6:]:
+    data = data.replace(i, ' ')
+
+while ' 0' in data:
+    data = data.replace(' 0', ' ')
+data = data.split()
+
+print(len(max(data, key=len)))
+
+
+
