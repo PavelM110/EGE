391dafb7444546be8270a713f463fc7c9653af63
PMolchanov <pavel1410071@gmail.com>
2025-05-19 16:25:40 +0200

    https://kompege.ru/variant?kim=25100502
    Task-24-3

--- Lessons/Year-25/May/19-05-25/Task-24-4643.py
+++ Lessons/Year-25/May/19-05-25/Task-24-4643.py
@@ -8,3 +8,9 @@ pat = r'(\d\d[AB])*'
 matches = finditer(pat, data)
 
 print(max([len(i.group()) for i in matches]) // 3)
+
+data = data.replace('2', '1').replace('B', 'A')
+data = data.replace('11A', '@').replace('A', ' ').replace('1', ' ')
+data = data.split()
+
+print(len(max(data, key=len)))
