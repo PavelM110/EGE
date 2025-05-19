fe3b61d6145d63ed3857d5104bcf9b8294d53b1b
PMolchanov <pavel1410071@gmail.com>
2025-05-19 16:08:23 +0200

    https://kompege.ru/variant?kim=25100502
    Task-5

---
+++ Lessons/Year-25/May/19-05-25/Task-5-4869.py
@@ -0,0 +1,12 @@
+for n in range(1, 10000):
+    r = bin(n)[2:]
+    ch_1 = 0
+    n_0 = 0
+    for p in range(0, len(r), 2):
+        if r[p] == '0': n_0 += 1
+    for p in range(1, len(r), 2):
+        if r[p] == '1': ch_1 += 1
+    r = abs(ch_1 - n_0)
+    if r == 5:
+        print(n)
+        break
