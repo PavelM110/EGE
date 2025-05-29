51b85f29ec898d00e9b4084d63a4a21d9227460d
PMolchanov <pavel1410071@gmail.com>
2025-05-29 10:23:37 +0200

    https://kompege.ru/variant?kim=25101705
    Task-26-8

---
+++ Lessons/Year-25/May/29-05-25/Task-26-1868.py
@@ -0,0 +1,12 @@
+with open('26_1868.txt') as file:
+    n = int(file.readline())
+    data = [list(map(int, i.split())) for i in file if i]
+
+data = sorted(data, key=lambda x: [-x[0], x[1]])
+
+for i in range(len(data) - 1):
+    r1, p1, r2, p2 = data[i][0], data[i][1], data[i+1][0], data[i+1][1]
+    if r1 == r2:
+        if p2 - p1 == 3:
+            print(r1, p1 + 1)
+            break
